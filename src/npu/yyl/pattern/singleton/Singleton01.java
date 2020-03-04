7f6900168272296ad53ed306e3d0243a7b7c7612
ylyang <yylin7239@163.com>
2020-03-05 01:25:48 +0800

    经典的单件模式实现

---
+++ src/npu/yyl/pattern/singleton/Singleton01.java
@@ -0,0 +1,30 @@
+package npu.yyl.pattern.singleton;
+
+/**
+ * 经典的单件模式实现
+ */
+public class Singleton01 {
+
+    // 利用一个静态变量来记录Singleton类的唯一实例
+    private static Singleton01 uniqueInstance;
+
+    /*
+        别的成员变量
+     */
+
+    // 把构造器声明为私有的，只有从Singleton类内部才可以调用构造器
+    public Singleton01() {
+    }
+
+    //
+    public static Singleton01 getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton01();
+        }
+        return uniqueInstance;
+    }
+
+    /*
+        别的方法实现
+     */
+}
